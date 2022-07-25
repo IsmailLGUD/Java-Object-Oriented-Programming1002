fd763c1c2599b0d5f4641f62d52b40f0132353ef
Ismail <mdismailhossain215@gmail.com>
2022-07-25 10:43:47 +0600

    Overriding Constructor or Overriding Method

---
+++ src/test/java/ClassMethodOOP01/ToStringMethod.java
@@ -0,0 +1,58 @@
+package ClassMethodOOP01;
+
+
+
+	class Tiger {
+		int legs;
+		String color, type;
+		boolean isPet;
+		int age;
+		float weight;
+
+		public Tiger() {
+			// default constructor
+			// System.out.println("default constructor");
+			legs = 4;
+		}
+
+		// Method Overloading
+		public Tiger(int legs, String c, float weight) {
+
+			this.legs = legs;
+			color = c;
+			this.weight = weight;
+
+		}
+
+		// Overriding toString()Method
+		public String toString() {
+			return "legs: " + legs + ", color: " + color + ", weight: " + weight;
+		}
+
+		void run() {
+
+			System.out.println("Dog runs!");
+		}
+
+		void eat() {
+
+		}
+
+		void sleep() {
+			System.out.println("Dog sleep!");
+
+		}
+	}
+	public class ToStringMethod {
+
+	public static void main(String[] args) {
+
+		Tiger d1 = new Tiger(3, "Black", 5);
+		System.out.println(d1);
+
+		Tiger t2 = new Tiger(4, "white", 6);
+		System.out.println(t2);
+
+	}
+
+}

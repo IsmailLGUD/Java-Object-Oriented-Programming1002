1947d5f1ab51ef633afae07ae1cfd8056a2df3ac
Ismail <mdismailhossain215@gmail.com>
2022-08-25 09:07:38 +0600

    Inheritance

---
+++ src/test/java/Inheritance/InheritanceClass.java
@@ -0,0 +1,19 @@
+package Inheritance;
+class Mammal{
+	  public float weight;
+	  public int i;
+	  
+	}
+
+	class Dog extends Mammal{
+	  //attributes and objects
+	}
+
+public class InheritanceClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
